2c30463a6d82826d681909af5e5760d8e75f78d5
Scott Lambie <000755529@mohawkcollege.ca>
2019-01-03 13:53:52 -0500

    First Commit

---
+++ WebApplication6/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication6._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <asp:TextBox ID="TextBoxScreen" runat="server" Height="50px" Width="75%" ReadOnly="True"></asp:TextBox>
+    </div>
+
+    <div class="row">
+        <div class="col-md-3">
+            <asp:Button ID="Button1" runat="server" Height="72px" OnClick="Button1_Click" Text="7" Width="191px" />
+            <asp:Button ID="ButtonFour" runat="server" Height="72px" Text="4" Width="191px" OnClick="ButtonFour_Click" />
+            <asp:Button ID="Button9" runat="server" Height="72px" Text="1" Width="191px" OnClick="Button9_Click" />
+            <asp:Button ID="Button13" runat="server" Height="72px" Text="0" Width="191px" OnClick="Button13_Click" />   
+        </div>
+        <div class="col-md-3">
+            <asp:Button ID="ButtonEight" runat="server" Height="72px" Text="8" Width="191px" OnClick="ButtonEight_Click" />
+            <asp:Button ID="ButtonFive" runat="server" Height="72px" Text="5" Width="191px" OnClick="ButtonFive_Click" />
+            <asp:Button ID="Button10" runat="server" Height="72px" Text="2" Width="191px" OnClick="Button10_Click" />
+            <asp:Button ID="ButtonDivide" runat="server" Height="72px" Text="/" Width="191px" OnClick="ButtonDivide_Click" />
+        </div>
+        <div class="col-md-3">
+            <asp:Button ID="ButtonNine" runat="server" Height="72px" Text="9" Width="191px" OnClick="ButtonNine_Click" />
+            <asp:Button ID="ButtonSix" runat="server" Height="72px" Text="6" Width="191px" OnClick="ButtonSix_Click" />
+            <asp:Button ID="Button11" runat="server" Height="72px" Text="3" Width="191px" OnClick="Button11_Click" />
+            <asp:Button ID="ButtonMultiply" runat="server" Height="72px" Text="X" Width="191px" OnClick="ButtonMultiply_Click" />
+            
+        </div>
+        <div class="col-md-3">
+            <asp:Button ID="ButtonCE" runat="server" Height="72px" Text="CE" Width="191px" OnClick="ButtonCE_Click" />
+            <asp:Button ID="ButtonAdd" runat="server" Height="72px" Text="+" Width="191px" OnClick="ButtonAdd_Click" />
+            <asp:Button ID="ButtonSubtract" runat="server" Height="72px" Text="-" Width="191px" OnClick="ButtonSubtract_Click" />
+            <asp:Button ID="ButtonEquals" runat="server" Height="72px" Text="=" Width="191px" OnClick="ButtonEquals_Click" />
+        </div>
+    </div>
+
+</asp:Content>
---
+++ WebApplication6/Default.aspx.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class _Default : Page
+    {
+
+        static int firstNumber;
+        static int secondNumber;
+        static int answer;
+        static float divideAnswer;
+        static char sign;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "7";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "7";
+            }
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "0";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "0";
+            }
+        }
+
+        protected void ButtonEight_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "8";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "8";
+            }
+        }
+
+        protected void ButtonNine_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "9";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "9";
+            }
+
+        }
+
+        protected void ButtonFour_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "4";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "4";
+            }
+        }
+
+        protected void ButtonFive_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "5";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "5";
+            }
+        }
+
+        protected void ButtonSix_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "6";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "6";
+            }
+        }
+
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "1";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "1";
+            }
+        }
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "2";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "2";
+            }
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if ((TextBoxScreen.Text == "+") || (TextBoxScreen.Text == "-") || (TextBoxScreen.Text == "X") || (TextBoxScreen.Text == "/"))
+            {
+                TextBoxScreen.Text = "";
+                TextBoxScreen.Text = TextBoxScreen.Text + "3";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text + "3";
+            }
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text != "")
+            {
+                firstNumber = int.Parse(TextBoxScreen.Text);
+                sign = '+';
+                TextBoxScreen.Text = "+";
+            }
+        }
+
+        protected void ButtonEquals_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text != "")
+            {
+                secondNumber = int.Parse(TextBoxScreen.Text);
+
+                if (sign == '+')
+                {
+                    answer = firstNumber + secondNumber;
+                    TextBoxScreen.Text = answer.ToString();
+                } else if (sign == '-')
+                {
+                    answer = firstNumber + secondNumber;
+                    TextBoxScreen.Text = answer.ToString();
+                } else if (sign == 'X')
+                {
+                    answer = firstNumber * secondNumber;
+                    TextBoxScreen.Text = answer.ToString();
+                } else if (sign == '/')
+                {
+                    if (secondNumber != 0)
+                    {
+                        divideAnswer = (float)firstNumber / (float)secondNumber;
+                        TextBoxScreen.Text = divideAnswer.ToString();
+                    }
+                    else
+                    {
+                        TextBoxScreen.Text = "Cannot Divide By Zero";
+                    }
+                }
+            }
+        }
+
+        protected void ButtonSubtract_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text != "")
+            {
+                firstNumber = int.Parse(TextBoxScreen.Text);
+                sign = '-';
+                TextBoxScreen.Text = "-";
+            }
+        }
+
+        protected void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text != "")
+            {
+                firstNumber = int.Parse(TextBoxScreen.Text);
+                sign = 'X';
+                TextBoxScreen.Text = "X";
+            }
+        }
+
+        protected void ButtonDivide_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text != "")
+            {
+                firstNumber = int.Parse(TextBoxScreen.Text);
+                sign = '/';
+                TextBoxScreen.Text = "/";
+            }
+        }
+
+        protected void ButtonCE_Click(object sender, EventArgs e)
+        {
+            TextBoxScreen.Text = "";
+        }
+    }
+}
